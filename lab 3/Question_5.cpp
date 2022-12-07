f8c2ed7ff16b045af41c3d6243998129341cf5d6
Rowaina <rowainaabdelnasser@gmail.com>
2022-12-07 03:44:51 +0200

    jihj\

--- lab 3/Question_5.cpp
+++ lab 3/Question_5.cpp
@@ -1,41 +1,49 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-void sieve(int n, bool primes[])
+long long fast_modular_exponentiaion(long long base, long long exp, long long mod)
 {
-    for(int i = 1; i <= n; i++) primes[i] = true;
-    for(int i = 2; i <= sqrt(n); i++)
-        {
-        if(primes[i])
-        {
-            for(int j = i * i; j <= n; j += i)
-            {
-                primes[j] = false;
-            }
-        }
+    long long bit = ceil(log2(exp)), res = 1, power = base % mod;
+    for(long long i = 0; i <= bit; i++)
+    {
+        //bit exists
+        if((long long)pow(2, i) & exp) res = res * power % mod;
+        power = power * power % mod;
     }
+    return res;
 }
 
-void trialDivisionFactorization(int n, bool primes[], vector<int>& factors)
+bool single_test(int m, int n)
 {
-    sieve(n, primes);
-    for(int i = 2; i <= n; i++)
-        {
-        if(primes[i] && n % i == 0)
-        {
-            n /= i;
-            factors.push_back(i--);
-        }
+    int a = 2 + rand() % (n - 4);
+    int res = fast_modular_exponentiaion(a, m, n);
+    if(res == 1 || res == n - 1) return true;
+    while(m != n - 1)
+    {
+        res = res * res % n;
+        m *= 2;
+        if(res == 1) return false;
+        else if(res == n - 1) return true;
     }
+    return false;
 }
 
-int main()
+bool millerTest(int n, int k)
 {
-    int n;
-    cout << "Enter number: ";
-    cin >> n;
-    bool primes[n + 1];
-    vector<int> factors;
-    trialDivisionFactorization(n, primes, factors);
-    for(int i = 0; i < factors.size(); i++) cout << factors[i] << ' ';
+    if(n == 1 || n == 4) return false;
+    else if(n == 2 || n == 3) return true;
+    int m = n - 1;
+    while(m % 2 == 0) m /= 2;
+    for(int i = 0; i < k; i++)
+        if(single_test(m, n)) return false;
+    return true;
+}
+
+int main ()
+{
+    int n, k;
+    cin >> n >> k;
+    if(!millerTest(n, k)) cout << "Probably prime";
+    else cout << "Not prime";
 }
