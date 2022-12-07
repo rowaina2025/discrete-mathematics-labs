72a774a4481707fa2bd7353a0aac97f8d583479b
Random <ehabflash833@gmail.com>
2022-12-07 02:06:34 +0200

    Update Question_5.cpp

--- lab 3/Question_5.cpp
+++ lab 3/Question_5.cpp
@@ -1,11 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void sieve(int n, bool primes[]) {
+void sieve(int n, bool primes[])
+{
     for(int i = 1; i <= n; i++) primes[i] = true;
-    for(int i = 2; i <= sqrt(n); i++) {
-        if(primes[i]) {
-            for(int j = i * i; j <= n; j += i) {
+    for(int i = 2; i <= sqrt(n); i++)
+        {
+        if(primes[i])
+        {
+            for(int j = i * i; j <= n; j += i)
+            {
                 primes[j] = false;
             }
         }
@@ -15,15 +19,18 @@ void sieve(int n, bool primes[]) {
 void trialDivisionFactorization(int n, bool primes[], vector<int>& factors)
 {
     sieve(n, primes);
-    for(int i = 2; i <= n; i++) {
-        if(primes[i] && n % i == 0) {
+    for(int i = 2; i <= n; i++)
+        {
+        if(primes[i] && n % i == 0)
+        {
             n /= i;
             factors.push_back(i--);
         }
     }
 }
 
-int main() {
+int main()
+{
     int n;
     cout << "Enter number: ";
     cin >> n;
