7bdd737d52557b16883e21a062c44d9e7a0429c2
Rowaina <rowainaabdelnasser@gmail.com>
2022-12-07 05:59:00 +0200

    sdv

--- lab 3/Question_5.cpp
+++ lab 3/Question_5.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+/*
+    patameters:
+    base: int
+    exp: int
+    mod: int
+    returns result of base^exp%mod using fast modular exponentiation algorithm
+*/
 int fastModularExponentiaion(int base, int exp, int mod)
 {
     int bit = ceil(log2(exp)), res = 1, power = base % mod;
@@ -14,13 +21,24 @@ int fastModularExponentiaion(int base, int exp, int mod)
     return res;
 }
 
+/*
+    parametes:
+    m: int
+    n: int
+    returns true if n is probably prime and false if composite for single base created randonly
+*/
 bool singleTest(int m, int n)
 {
+    //creates random base a
     int a = 2 + rand() % (n - 4);
+    //computes a ^ m % n with fast modular exponentiation
     int res = fastModularExponentiaion(a, m, n);
+    //prime if the result is 1 or n - 1
     if(res == 1 || res == n - 1) return true;
+    //loop stops when m truns back to its original value n - 1
     while(m != n - 1)
     {
+        //try make res reaches n - 1 or 1 to decide whether true or false by multiplying res by itself with mod n
         res = res * res % n;
         m *= 2;
         if(res == 1) return false;
@@ -29,6 +47,13 @@ bool singleTest(int m, int n)
     return false;
 }
 
+/*
+    parameters:
+    n: int
+    k: int
+    test number n k of times, if passes all tests, returns true indicating probably prime 
+    and false if fails one test al least indicating composite
+*/
 bool millerTest(int n, int k)
 {
     if(n == 1 || n == 4) return false;
